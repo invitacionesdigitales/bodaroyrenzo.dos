60fbdd440fef64a2a0fc4b397e9d7b545237f41d
invitacionesdigitales <111189010+invitacionesdigitales@users.noreply.github.com>
2025-11-12 17:12:10 -0300

    uno

---
+++ main.js
@@ -0,0 +1,106 @@
+
+   
+let diaEvento = new Date('February 07 2026, 2026 21:30:00').getTime();
+let boton = document.querySelector("#btn")
+
+
+
+// diaEvento = new Date().getTime() + 1_000 * 20;
+
+let relojE = document.getElementById('reloj');
+
+const diaM = 86_400_000;
+const horaM = 3_600_000;
+const minutoM = 60_000;
+const segundoM = 1_000;
+const ejecutarReloj = setInterval(
+    () => {
+
+        let hoy = new Date().getTime();
+        let distancia = diaEvento - hoy;
+
+        let diaR = Math.trunc(distancia / diaM);
+
+        let horaR = Math.trunc((distancia % diaM) / horaM);
+
+        let minutoR = Math.trunc((distancia % horaM) / minutoM);
+
+        let segundoR = Math.trunc((distancia % minutoM) / segundoM);
+
+        if (distancia  < 1) {
+            clearInterval(ejecutarReloj);
+            diaEventoAhora();
+        }
+
+        pintarElementos(diaR, horaR, minutoR, segundoR);
+
+    }
+, 1000);
+
+let creDia = document.getElementById('cre-dia');
+let creHora = document.getElementById('cre-hora');
+let creMinuto = document.getElementById('cre-minuto');
+let creSegundo = document.getElementById('cre-segundo');
+function pintarElementos(dia, hora, minuto, segundo) {
+
+    segundo = (segundo < 10) ? '0' + segundo : segundo;
+
+    minuto = (minuto < 10) ? '0' + minuto : minuto;
+
+    hora = (hora < 10) ? '0' + hora : hora;
+
+    creDia.innerHTML = dia;
+    creHora.innerHTML = hora;
+    creMinuto.innerHTML = minuto;
+    creSegundo.innerHTML = segundo;
+}
+
+function diaEventoAhora () {
+    alert('Dia del Evento!!!');
+    // Todo el codigo cuando el evento llegue
+}
+
+
+
+
+
+
+  boton.addEventListener("click", () => {
+
+    Swal.fire({
+      title: '<strong>VER LA UBICACION<u></u></strong>',
+      icon: 'success',
+      html:
+        'INGRESÁ' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3291.6104571726864!2d-58.69177942489554!3d-34.41124584735772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bca1767a9f4b37%3A0xfcb2a42d467e7a0d!2sParroquia%20Nuestra%20Se%C3%B1ora%20del%20Carmen!5e0!3m2!1ses-419!2sar!4v1722640886520!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+        
+        
+    })
+  });
+   
+   
+  function exito(){
+    Swal.fire({
+        title: '<strong>VER LA UBICACION<u></u></strong>',
+        icon: 'success',
+        html:
+          'INGRESÁ' +
+          '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3561.447127642039!2d-65.24192661269254!3d-26.79388806090984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d08ec4754c9%3A0x44185721dc00dbfe!2sCastro%20Barros%202090%2C%20T4000%20San%20Miguel%20de%20Tucum%C3%A1n%2C%20Tucum%C3%A1n!5e0!3m2!1ses!2sar!4v1762816464754!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+          'Podes ampliar el mapa',
+          
+          
+      })
+}
+   
+   
+   
+   
+  
+
+    
+    
+
+   
+        
+    
+    
